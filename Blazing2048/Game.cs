106d56afdf1935077c4c79942e2d9ce47bdad510
tomd10 <tomas.chabada@seznam.cz>
2024-12-07 21:03:02 +0100

    Added Game class + properties/fields.

---
+++ Blazing2048/Game.cs
@@ -0,0 +1,23 @@
+﻿namespace Blazing2048
+{
+    public abstract class Game
+    {
+        protected Random rnd = new Random();
+
+        public int[,] gameBoard { get; protected set; }
+        public int[,] previousGameBoard { get; protected set; } = null;
+
+        protected int dimX;
+        protected int dimY;
+        protected int spawnCount;
+
+        //protected bool isWon = false;
+
+        public bool isWon { get; protected set; } = false;
+        public bool wonFlag { get; protected set; } = false;
+
+        public bool isLost { get; protected set; } = false;
+
+        public int score { get; protected set; }
+    }
+}

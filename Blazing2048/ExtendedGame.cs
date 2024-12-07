bc4c3bbb435e2da190603297904c1e99983c0e85
tomd10 <tomas.chabada@seznam.cz>
2024-12-07 21:10:30 +0100

    Added parental Standard and ExtendedGame classes.

---
+++ Blazing2048/ExtendedGame.cs
@@ -0,0 +1,23 @@
+﻿namespace Blazing2048
+{
+    public class ExtendedGame : Game
+    {
+        public ExtendedGame(int dimX, int dimY, int spawnCount)
+        {
+            this.dimX = dimX;
+            this.dimY = dimY;
+            this.spawnCount = spawnCount;
+            gameBoard = new int[dimX, dimY];
+
+            for (int i = 0; i < dimX; i++)
+            {
+                for (int j = 0; j < dimY; j++)
+                {
+                    gameBoard[i, j] = 0;
+                }
+            }
+
+            GenerateTiles(2);
+        }
+    }
+}
---
+++ Blazing2048/StandardGame.cs
@@ -0,0 +1,25 @@
+﻿namespace Blazing2048
+{
+    public class StandardGame : Game
+    {
+
+        public StandardGame()
+        {
+            score = 0;
+            dimX = 4;
+            dimY = 4;
+            spawnCount = 1;
+            gameBoard = new int[dimX, dimY];
+
+            for (int i = 0; i < dimX; i++)
+            {
+                for (int j = 0; j < dimY; j++)
+                {
+                    gameBoard[i, j] = 0;
+                }
+            }
+
+            GenerateTiles(2);
+        }
+    }
+}
